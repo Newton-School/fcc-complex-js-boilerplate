d892662511e5fafb8534e7276c7f514977f33b84
Akshat Sethi <masterakshata@gmail.com>
2022-05-23 01:32:59 +0530

    done

---
+++ src/components/JSX.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import '../styles/App.css';
+
+
+// write jsx, not a component
+const JSX = <div id="jsx">ok</div>
+
+
+
+export default JSX;
---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import JSX from "./components/JSX";
+
+function App(){
+
+  
+
+    return JSX
+}
+
+ReactDOM.render(<App />, document.getElementById("root"));
